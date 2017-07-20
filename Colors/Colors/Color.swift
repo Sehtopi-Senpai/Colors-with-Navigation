c739ae1abe8fb30fad0e1b1622e50ebc8ba609ff
Josh Green <jg229@mail.missouri.edu>
2017-07-20 15:54:39 -0500

    added all the stuffz

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors-with-navigation
+//
+//  Created by Lappy on 7/20/17.
+//  Copyright © 2017 xanda code. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+    
+}
